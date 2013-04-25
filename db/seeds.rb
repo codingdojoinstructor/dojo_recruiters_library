b44abdc97870a0acb69389b2791313b36329ef00
Mikey <root@macdevin-N68S3B.(none)>
2013-04-25 11:46:43 +0800

    Added student info in the seeds.rb to be created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,179 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# name: string
+# email: string
+# password: string
+# level: integer
+# location: string
+# hired_date: datetime
+# hired_company: string
+
+eva_roa = Student.new(
+	:name => "Eva Roa",
+	:email => "evabungle@gmail.com",
+	:password => "eva_cd1234"
+)
+eva_roa.save!
+
+jt_platt = Student.new(
+	:name => "James T. Platt",
+	:email => "James.T.Platt@gmail.com",
+	:password => "jtp_cd1234"
+)
+jt_platt.save!
+
+peter_whitfield = Student.new(
+	:name => "Peter Whitfield",
+	:email => "petergwhitfield@gmail.com",
+	:password => "peter_cd1234"
+)
+peter_whitfield.save!
+
+tamara_solorzano = Student.new(
+	:name => "Tamara Solorzano",
+	:email => "st.tamara@gmail.com",
+	:password => "tamara_cd1234"
+)
+tamara_solorzano.save!
+
+cory_whiteland = Student.new(
+	:name => "Cory Whiteland",
+	:email => "corywhiteland@gmail.com",
+	:password => "cory_cd1234"
+)
+cory_whiteland.save!
+
+william_quan = Student.new(
+	:name => "William Quan",
+	:email => "williamuquan@gmail.com",
+	:password => "william_cd1234"
+)
+william_quan.save!
+
+dilys_sun = Student.new(
+	:name => "Dilys Sun",
+	:email => "optanovo@gmail.com",
+	:password => "dilys_cd1234"
+)
+dilys_sun.save!
+
+ian_dahlberg = Student.new(
+	:name => "Ian Dahlberg",
+	:email => "ijdahlberg@gmail.com",
+	:password => "ian_cd1234"
+)
+ian_dahlberg.save!
+
+casey_mccalister = Student.new(
+	:name => "Casey McCallister",
+	:email => "casey.mccallister@gmail.com",
+	:password => "casey_cd1234"
+)
+casey_mccalister.save!
+
+bert_mcguirk = Student.new(
+	:name => "Bert Greg Mcguirk",
+	:email => "bert.mcguirk@gmail.com",
+	:password => "bert_cd1234"
+)
+bert_mcguirk.save!
+
+aarthi_rajiv = Student.new(
+	:name => "Aarthi Rajiv",
+	:email => "aarthi_career@gmail.com",
+	:password => "aarthi_cd1234"
+)
+aarthi_rajiv.save!
+
+rick_sanchez = Student.new(
+	:name => "Rick Sanchez",
+	:email => "r_san56@yahoo.com",
+	:password => "rick_cd1234"
+)
+rick_sanchez.save!
+
+eylem_ozaslan = Student.new(
+	:name => "Eylem Ozaslan",
+	:email => "eylem.ozaslan@gmail.com",
+	:password => "eylem_cd1234"
+)
+eylem_ozaslan.save!
+
+angie_rangel = Student.new(
+	:name => "Angie Rangel",
+	:email => "heyrangel@yahoo.com",
+	:password => "angie_cd1234"
+)
+angie_rangel.save!
+
+jeremy_shoenig = Student.new(
+	:name => "Jeremy Shoenig",
+	:email => "jshoenig@hotmail.com",
+	:password => "jeremy_cd1234"
+)
+jeremy_shoenig.save!
+
+victor_tran = Student.new(
+	:name => "Victor Tran",
+	:email => "vicfortran@gmail.com",
+	:password => "victor_cd1234"
+)
+victor_tran.save!
+
+john_theodore = Student.new(
+	:name => "John Theodore",
+	:email => "JohnTheodore42@gmail.com",
+	:password => "jtheodore_cd1234"
+)
+john_theodore.save!
+
+miles_fink = Student.new(
+	:name => "Miles Fink",
+	:email => "mfinkca@gmail.com",
+	:password => "miles_cd1234"
+)
+miles_fink.save!
+
+scottie_bryant = Student.new(
+	:name => "Scottie Bryant",
+	:email => "sbphilly@hotmail.com",
+	:password => "scottie_cd1234"
+)
+scottie_bryant.save!
+
+randall_frisk = Student.new(
+	:name => "Randall Frisk",
+	:email => "friskslalom6@gmail.com",
+	:password => "randall_cd1234"
+)
+randall_frisk.save!
+
+marcos_guimaraes = Student.new(
+	:name => "Marcos Guimaraes",
+	:email => "markuz.br@gmail.com",
+	:password => "marcos_cd1234"
+)
+marcos_guimaraes.save!
+
+ben_jones = Student.new(
+	:name => "Ben Jones",
+	:email => "CompanionxCube@gmail.com",
+	:password => "ben_cd1234"
+)
+ben_jones.save!
+
+samson_gowon = Student.new(
+	:name => "Samson L. Gowon",
+	:email => "glongi@hotmail.com",
+	:password => "samson_cd1234"
+)
+samson_gowon.save!
+
+
+jose_luigi_flores = Student.new(
+	:name => "Jose Luigi Flores",
+	:email => "Joseflores8082@gmail.com",
+	:password => "jose_luigi_cd1234"
+)
+jose_luigi_flores.save!
