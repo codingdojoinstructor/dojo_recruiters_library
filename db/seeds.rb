eec76da2809b52293d9859e6bdbff8ec6fab87e4
mbuyco@village88.com <mac-devin@macdevin-N68S3B.(none)>
2013-04-26 22:03:51 +0800

    Update seeds.rb and made some minor UI changes on student profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,93 +13,73 @@
 # hired_date: datetime
 # hired_company: string
 
-eva_roa = Student.find_by_email("evabungle@gmail.com")
-eva_roa.student_profile.image_src = "/assets/profile_pictures/Eva.jpg"
-eva_roa.student_profile.save!
+# eva_roa = Student.find_by_email("evabungle@gmail.com")
 
-jt_platt = Student.find_by_email("James.T.Platt@gmail.com")
-jt_platt.student_profile.image_src = "/assets/profile_pictures/JT Platt3.jpg"
-jt_platt.student_profile.save!
+# jt_platt = Student.find_by_email("James.T.Platt@gmail.com")
 
-peter_whitfield = Student.find_by_email("petergwhitfield@gmail.com")
-peter_whitfield.student_profile.image_src = "/assets/profile_pictures/Peter2.jpg"
-peter_whitfield.student_profile.save!
+# peter_whitfield = Student.find_by_email("petergwhitfield@gmail.com")
 
-# tamara_solorzano = Student.find_by_email("st.tamara@gmail.com")
+tamara_solorzano = Student.find_by_email("st.tamara@gmail.com")
+red_belt = tamara_solorzano.student_belts.new
+red_belt.belt_id = 4
+red_belt.score = 10
+red_belt.save!
 
-cory_whiteland = Student.find_by_email("corywhiteland@gmail.com")
-cory_whiteland.student_profile.image_src = "/assets/profile_pictures/Cory.jpg"
-cory_whiteland.student_profile.save!
+# cory_whiteland = Student.find_by_email("corywhiteland@gmail.com")
 
-william_quan = Student.find_by_email("williamuquan@gmail.com")
-william_quan.student_profile.image_src = "/assets/profile_pictures/William.jpg"
-william_quan.student_profile.save!
+# william_quan = Student.find_by_email("williamuquan@gmail.com")
 
 dilys_sun = Student.find_by_email("optanovo@gmail.com")
-dilys_sun.student_profile.image_src = "/assets/profile_pictures/Dilys2.jpg"
-dilys_sun.student_profile.save!
+red_belt, black_belt = dilys_sun.student_belts.new, dilys_sun.student_belts.new
+red_belt.belt_id = 4
+red_belt.score = 9.5
+red_belt.save!
+black_belt.belt_id = 5
+black_belt.score = 8
+black_belt.save!
 
 ian_dahlberg = Student.find_by_email("ijdahlberg@gmail.com")
-ian_dahlberg.student_profile.image_src = "/assets/profile_pictures/Ian.jpg"
-ian_dahlberg.student_profile.save!
+red_belt = ian_dahlberg.student_belts.new
+red_belt.belt_id = 4
+red_belt.score = 9
+red_belt.save!
 
-casey_mccalister = Student.find_by_email("casey.mccallister@gmail.com")
-casey_mccalister.student_profile.image_src = "/assets/profile_pictures/Casey2.jpg"
-casey_mccalister.student_profile.save!
+# casey_mccalister = Student.find_by_email("casey.mccallister@gmail.com")
 
-bert_mcguirk = Student.find_by_email("bert.mcguirk@gmail.com")
-bert_mcguirk.student_profile.image_src = "/assets/profile_pictures/Greg.jpg"
-bert_mcguirk.student_profile.save!
+# bert_mcguirk = Student.find_by_email("bert.mcguirk@gmail.com")
 
 # aarthi_rajiv = Student.find_by_email("aarthi_career@gmail.com")
 
-rick_sanchez = Student.find_by_email("r_san56@yahoo.com")
-rick_sanchez.student_profile.image_src = "/assets/profile_pictures/Rick2.jpg"
-rick_sanchez.student_profile.save!
+# rick_sanchez = Student.find_by_email("r_san56@yahoo.com")
 
 eylem_ozaslan = Student.find_by_email("eylem.ozaslan@gmail.com")
-eylem_ozaslan.student_profile.image_src = "/assets/profile_pictures/Eylem.jpg"
-eylem_ozaslan.student_profile.save!
+green_belt = eylem_ozaslan.student_belts.new
+green_belt.belt_id = 3
+green_belt.score = 9.5
+green_belt.save!
 
-angie_rangel = Student.find_by_email("heyrangel@yahoo.com")
-angie_rangel.student_profile.image_src = "/assets/profile_pictures/Angie.jpg"
-angie_rangel.student_profile.save!
+# angie_rangel = Student.find_by_email("heyrangel@yahoo.com")
 
 # jeremy_shoenig = Student.find_by_email("jshoenig@hotmail.com")
 
-victor_tran = Student.find_by_email("vicfortran@gmail.com")
-victor_tran.student_profile.image_src = "/assets/profile_pictures/Victor2.jpg"
-victor_tran.student_profile.save!
+# victor_tran = Student.find_by_email("vicfortran@gmail.com")
 
-john_theodore = Student.find_by_email("JohnTheodore42@gmail.com")
-john_theodore.student_profile.image_src = "/assets/profile_pictures/JT Theodore.jpg"
-john_theodore.student_profile.save!
+# john_theodore = Student.find_by_email("JohnTheodore42@gmail.com")
 
-miles_fink = Student.find_by_email("mfinkca@gmail.com")
-miles_fink.student_profile.image_src = "/assets/profile_pictures/Miles3.jpg"
-miles_fink.student_profile.save!
+# miles_fink = Student.find_by_email("mfinkca@gmail.com")
 
-scottie_bryant = Student.find_by_email("sbphilly@hotmail.com")
-scottie_bryant.student_profile.image_src = "/assets/profile_pictures/Scott.jpg"
-scottie_bryant.student_profile.save!
+# scottie_bryant = Student.find_by_email("sbphilly@hotmail.com")
 
-randall_frisk = Student.find_by_email("friskslalom6@gmail.com")
-randall_frisk.student_profile.image_src = "/assets/profile_pictures/Randall.jpg"
-randall_frisk.student_profile.save!
+# randall_frisk = Student.find_by_email("friskslalom6@gmail.com")
 
-marcos_guimaraes = Student.find_by_email("markuz.br@gmail.com")
-marcos_guimaraes.student_profile.image_src = "/assets/profile_pictures/Markuz.jpg"
-marcos_guimaraes.student_profile.save!
+# marcos_guimaraes = Student.find_by_email("markuz.br@gmail.com")
 
 ben_jones = Student.find_by_email("CompanionxCube@gmail.com")
-ben_jones.student_profile.image_src = "/assets/profile_pictures/Ben2.jpg"
-ben_jones.student_profile.save!
+red_belt = ben_jones.student_belts.new
+red_belt.belt_id = 4
+red_belt.score = 7.5
+red_belt.save!
 
-samson_gowon = Student.find_by_email("glongi@hotmail.com")
-samson_gowon.student_profile.image_src = "/assets/profile_pictures/Samson.jpg"
-samson_gowon.student_profile.save!
+# samson_gowon = Student.find_by_email("glongi@hotmail.com")
 
-
-jose_luigi_flores = Student.find_by_email("Joseflores8082@gmail.com")
-jose_luigi_flores.student_profile.image_src = "/assets/profile_pictures/Jose2.jpg"
-jose_luigi_flores.student_profile.save!
+# jose_luigi_flores = Student.find_by_email("Joseflores8082@gmail.com")
