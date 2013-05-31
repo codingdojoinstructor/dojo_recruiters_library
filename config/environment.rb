e2dc455007aa5a20a0bed1e2605b9edf52f82bcf
Noah Jerreel Guillen <nguillen@village88.com>
2013-05-31 18:10:37 +0800

    add email validation in change password and remove domain in the email config for Heroku issue on sending email

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,6 @@ Codingdojo::Application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
-    :domain               => 'gmail.com',
     :user_name            => 'nguillen@village88.com',
     :password             => 'njguillen',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+    :authentication       => 'plain' }
