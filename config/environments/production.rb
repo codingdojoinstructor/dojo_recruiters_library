80c77cedd8f51f186184e3ab7fe9e835747e48fe
Noah Jerreel Guillen <nguillen@village88.com>
2013-05-31 19:55:19 +0800

    fixing mailing service in heroku server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,18 +75,13 @@ Codingdojo::Application.configure do
       }
   }
 
-  config.action_mailer.default_url_options = { :host => 'codingdojo.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "ssl://smtp.googlemail.com",
-    port: 587,
-    domain: "codingdojo.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "codingdojo.herokuapp.com",
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
   }
 end
