e33e904bc2ed7dd89c7d24d8a44cfb439d43332a
Noah Jerreel Guillen <nguillen@village88.com>
2013-06-03 10:42:44 +0800

    reverting changes. fixing email service on heroku server using gmail and action mailer

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module Codingdojo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Codingdojo::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {  
-  	:address              => "smtp.gmail.com",  
-  	:tls 				  => true,
-  	:port                 => 587,  
- 	:user_name => ENV['GMAIL_USERNAME'],
-	:password =>  ENV['GMAIL_PASSWORD'], 
-	:authentication => "plain",
-	:enable_starttls_auto => true
-}
